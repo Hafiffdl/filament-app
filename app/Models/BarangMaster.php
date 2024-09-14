87522fbc5497a15cd4461acffc2b284bb2c300ef
Hafiffdl <hafif.fdl16@gmail.com>
2024-09-14 17:32:38 +0700

    add model barang master

---
+++ app/Models/BarangMaster.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BarangMaster extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nama_barang', 'nomor_batch', 'kadaluarsa', 'harga_satuan', 'satuan', 'sumber_dana', 'stock'];
+
+    // Relasi ke BarangTransaksi
+    public function barangTransaksis()
+    {
+        return $this->hasMany(BarangTransaksi::class);
+    }
+}
