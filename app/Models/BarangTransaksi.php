92cd8ea1897396223985080d3871c2a164e28d33
Hafiffdl <hafif.fdl16@gmail.com>
2024-09-14 17:32:51 +0700

    add model barang transaksi

---
+++ app/Models/BarangTransaksi.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BarangTransaksi extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['barang_master_id', 'jumlah', 'total_harga', 'kadaluarsa'];
+
+    public function barangMaster()
+    {
+        return $this->belongsTo(BarangMaster::class);
+    }
+
+    protected static function booted()
+    {
+        static::saving(function ($transaksi) {
+            $barang = $transaksi->barangMaster;
+            if ($barang) {
+                $transaksi->total_harga = $transaksi->jumlah * $barang->harga_satuan;
+
+                // Ensure stock does not go negative
+                if ($barang->stock < $transaksi->jumlah) {
+                    throw new \Exception("Stock tidak cukup untuk transaksi ini.");
+                }
+
+                // Update stock
+                $barang->stock -= $transaksi->jumlah;
+                $barang->save();
+            }
+        });
+    }
+}
