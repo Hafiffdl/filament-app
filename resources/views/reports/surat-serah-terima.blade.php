ebd02c493e5cd18ecbf41d33662af5f01d9e3fc1
Hafiffdl <hafif.fdl16@gmail.com>
2024-10-08 18:09:40 +0700

    update view data surat serah terima blade

--- resources/views/reports/surat-serah-terima.blade.php
+++ resources/views/reports/surat-serah-terima.blade.php
@@ -14,7 +14,7 @@
             text-align: center;
             margin-bottom: 20px;
             position: relative;
-            height: 110px; /* Adjust based on your logo height */
+            height: 110px;
         }
         .logo {
             position: absolute;
@@ -28,7 +28,6 @@
             width: 100%;
         }
         .title {
-            /* font-weight: bold; */
             font-size: 12px;
             margin: 2px 0;
         }
@@ -69,12 +68,12 @@
         }
         .subtotal-row {
             font-weight: bold;
-            background-color: #e0e0e0; /* Light grey background for the subtotal row */
+            background-color: #e0e0e0;
         }
         .subtotal-label {
             text-align: right;
-            vertical-align: bottom; /* Align text at the bottom of the cell */
-            padding-bottom: 5px; /* Lower the text slightly */
+            vertical-align: bottom;
+            padding-bottom: 5px;
         }
         .signature-section {
             display: flex;
@@ -100,7 +99,7 @@
             <div class="title">PEMERINTAH PROVINSI DAERAH KHUSUS IBUKOTA JAKARTA</div>
             <div class="title">DINAS PEMBERDAYAAN, PERLINDUNGAN ANAK DAN PENGENDALIAN PENDUDUK</div>
             <div class="title" style="font-weight: bold;">SUKU DINAS PEMBERDAYAAN PERLINDUNGAN ANAK DAN</div>
-            <div class="title"style="font-weight: bold;">PENGENDALIAN PENDUDUK KOTA ADMINISTRASI JAKARTA TIMUR</div>
+            <div class="title" style="font-weight: bold;">PENGENDALIAN PENDUDUK KOTA ADMINISTRASI JAKARTA TIMUR</div>
             <div>JL. PAHLAWAN REVOLUSI, PONDOK BAMBU</div>
             <div>TELP. : 8612584, FAX. : 8612584</div>
             <div>JAKARTA</div>
@@ -113,9 +112,9 @@
         BERITA ACARA<br>SERAH TERIMA BARANG PERSEDIAAN<br>ALAT/OBAT KONTRASEPSI DAN NON KONTRASEPSI
     </div>
 
-    <div class="document-number">Nomor: {{ $suratSerahTerima->spmb_nomor ?? 'N/A' }}</div>
+    <div class="document-number">Nomor: {{ $suratSerahTerima->nomor }}</div>
 
-    <p>Pada hari ini Selasa tanggal 4 bulan Juni tahun dua ribu dua puluh empat, di Jakarta, kami yang bertanda tangan di bawah ini :</p>
+    <p>Pada hari ini {{ \Carbon\Carbon::parse($suratSerahTerima->tanggal)->isoFormat('dddd') }} tanggal {{ \Carbon\Carbon::parse($suratSerahTerima->tanggal)->isoFormat('D') }} bulan {{ \Carbon\Carbon::parse($suratSerahTerima->tanggal)->isoFormat('MMMM') }} tahun {{ \Carbon\Carbon::parse($suratSerahTerima->tanggal)->isoFormat('Y') }}, di Jakarta, kami yang bertanda tangan di bawah ini :</p>
 
     <ol>
         <li>
@@ -149,39 +148,43 @@
             <th>JUMLAH</th>
         </tr>
         @foreach($suratSerahTerima->barangTransaksis as $transaksi)
-        <tr>
-            <td>{{ $loop->iteration }}</td>
-            <td>{{ $transaksi->barangMaster->nama_barang }}</td>
-            <td>{{ $transaksi->jumlah }}</td>
-            <td>{{ $transaksi->barangMaster->satuan }}</td>
-            <td>Rp {{ number_format($transaksi->barangMaster->harga_satuan, 2, ',', '.') }}</td>
-            <td>Rp {{ number_format($transaksi->total_harga, 2, ',', '.') }}</td>
-        </tr>
+            @foreach($transaksi->items as $item)
+                <tr>
+                    <td>{{ $loop->parent->iteration }}.{{ $loop->iteration }}</td>
+                    <td>{{ $item->barangMaster->nama_barang }}</td>
+                    <td>{{ $item->jumlah }}</td>
+                    <td>{{ $item->barangMaster->satuan }}</td>
+                    <td>Rp {{ number_format($item->barangMaster->harga_satuan, 2, ',', '.') }}</td>
+                    <td>Rp {{ number_format($item->total_harga, 2, ',', '.') }}</td>
+                </tr>
+            @endforeach
         @endforeach
         <tr class="subtotal-row">
             <td colspan="2" class="subtotal-label">SUB TOTAL</td>
             <td colspan="3"></td>
-            <td>Rp {{ number_format($suratSerahTerima->barangTransaksis->sum('total_harga'), 2, ',', '.') }}</td>
+            <td>Rp{{ number_format($suratSerahTerima->barangTransaksis->flatMap(fn($transaksi) => $transaksi->items)->sum('total_harga'), 2, ',', '.') }}</td>
         </tr>
     </table>
 
     <p>Demikian Berita Acara serah terima Barang Persediaan ini dibuat untuk dipergunakan sebagaimana mestinya</p>
+
     <div class="signature-section">
-    <table style="width: 100%; border: none;">
-        <tr>
-            <td style="width: 50%; text-align: center; border: none;">
-                <p><strong>PIHAK PERTAMA</strong><br>Pengurus Barang Pembantu<br>Sudin PPAPP Kota Adm. Jakarta Timur</p>
-                <br><br><br>
-                <p>Winda Ulaya Hanafi<br>NIP. 197305021992032001</p>
-            </td>
-            <td style="width: 50%; text-align: center; border: none;">
-                <p><strong>PIHAK KEDUA</strong><br>Pengurus Barang Pembantu<br>{{ $suratSerahTerima->faskes->nama ?? 'N/A' }}</p>
-                <br><br><br>
-                <p>{{ $suratSerahTerima->faskes->nama_pengurus_barang ?? 'N/A' }}<br>NIP.{{ $suratSerahTerima->faskes->nip_pengurus_barang ?? 'N/A' }}</p>
-            </td>
-        </tr>
-    </table>
-</div>
+        <table style="width: 100%; border: none;">
+            <tr>
+                <td style="width: 50%; text-align: center; border: none;">
+                    <p><strong>PIHAK PERTAMA</strong><br>Pengurus Barang Pembantu<br>Sudin PPAPP Kota Adm. Jakarta Timur</p>
+                    <br><br><br>
+                    <p>Winda Ulaya Hanafi<br>NIP. 197305021992032001</p>
+                </td>
+                <td style="width: 50%; text-align: center; border: none;">
+                    <p><strong>PIHAK KEDUA</strong><br>Pengurus Barang Pembantu<br>{{ $suratSerahTerima->faskes->nama ?? 'N/A' }}</p>
+                    <br><br><br>
+                    <p>{{ $suratSerahTerima->faskes->nama_pengurus_barang ?? 'N/A' }}<br>NIP. {{ $suratSerahTerima->faskes->nip_pengurus_barang ?? 'N/A' }}</p>
+                </td>
+            </tr>
+        </table>
+    </div>
+
     <div class="center-signature">
         <p>Kepala Sudin Pemberdayaan, Perlindungan Anak<br>
         dan Pengendalian Penduduk<br>
