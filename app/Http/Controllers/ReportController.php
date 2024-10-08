de060691bc1511f80beb0594d5b0465eaff944d6
Hafiffdl <hafif.fdl16@gmail.com>
2024-10-08 18:10:03 +0700

    update report controller

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -8,22 +8,18 @@ use Barryvdh\DomPDF\Facade\Pdf as PDF;
 
 class ReportController extends Controller
 {
-    public function printSuratKeluar($id)
+        public function printSuratKeluar($id)
     {
-        $suratKeluar = SuratKeluar::with('barangTransaksis.barangMaster')->findOrFail($id);
-
-            $pdf = PDF::loadView('reports.surat-keluar', compact('suratKeluar'));
-
-            return $pdf->stream('surat-keluar' . $suratKeluar->nomor . '.pdf');
+        $suratKeluar = SuratKeluar::with(['faskes', 'barangTransaksis.items.barangMaster'])->findOrFail($id);
+        $pdf = PDF::loadView('reports.surat-keluar', compact('suratKeluar'));
+        return $pdf->stream('surat-keluar-' . $suratKeluar->nomor . '.pdf');
     }
 
     public function printSuratSerahTerima($id)
     {
-        $suratSerahTerima = SuratKeluar::with('barangTransaksis.barangMaster')->findOrFail($id);
-
-            $pdf = PDF::loadView('reports.surat-serah-terima', compact('suratSerahTerima'));
-
-            return $pdf->stream('surat-serah-terima' . $suratSerahTerima->nomor . '.pdf');
+        $suratSerahTerima = SuratKeluar::with(['faskes', 'barangTransaksis.items.barangMaster'])->findOrFail($id);
+        $pdf = PDF::loadView('reports.surat-serah-terima', compact('suratSerahTerima'));
+        return $pdf->stream('surat-serah-terima-' . $suratSerahTerima->nomor . '.pdf');
     }
 }
 
