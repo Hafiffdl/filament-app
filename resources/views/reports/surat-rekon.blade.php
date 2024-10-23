c6e29790e6a167b13d81e02bf420fda17a27aa0e
Hafiffdl <hafif.fdl16@gmail.com>
2024-10-23 14:39:36 +0700

    update title view blade

--- resources/views/reports/surat-rekon.blade.php
+++ resources/views/reports/surat-rekon.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Berita Acara Serah Terima Barang Persediaan</title>
+    <title>Berita Acara Serah Terima Rekon</title>
     <style>
         body {
             font-family: Arial, sans-serif;
